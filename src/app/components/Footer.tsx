0498ef9dac95fa780754350f465d7bed61a578dd
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 00:39:16 +0100

    make Footer a component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div id="footer" className="container-fluid mt-md-3 py-2 bg-body-tertiary">
+      <p className="text-center mb-0">
+        This website is operated by Van Mildert College Junior Common Room,
+        a charity (CIO) registered in England and Wales
+        (<a href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5161238"
+            target="_blank">1190990</a>).
+        Copyright Van Mildert College JCR.
+      </p>
+    </div>
+  )
+}
