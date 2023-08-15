302f430247f8039378b01d4a60a837ab13144374
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 00:40:40 +0100

    update root layout with fragment components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import "./globals.css";
+import "./globals.scss";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
+import Head from "./components/Head";
+import Footer from "./components/Footer";
+import Foot from "./components/Foot";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,25 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-bs-theme="dark">
-      {/* Temp Head */}
-      <head>
-        {/* IMPORT FROM head.html */}
-        {/* <!-- Required meta tags --> */}
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* <!-- Bootstrap CSS --> */}
-        {/* <!-- CSS only --> */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
-        <noscript>Please enable JavaScript.</noscript>
-        {/* IMPORT FROM /assets/include/FontAwesome.html */}
-        <script src="https://kit.fontawesome.com/d07da7e4d9.js" crossOrigin="anonymous" async></script>
-        <link href="/assets/css/mildert.css" rel="stylesheet" />
-      </head>
-      {/* End Temp Head */}
+      <Head />
       <body className={inter.className + " pt-md-3"}>
         <Navbar />
         <div className="d-flex align-items-center text-center" style={{minHeight: "calc(100vh - 105px)"}}>
@@ -42,23 +27,8 @@ export default function RootLayout({
             {children}
           </div>
         </div>
-        {/* IMPORT FROM footer.html */}
-        <div id="footer" className="container-fluid mt-md-3 py-2 bg-body-tertiary">
-            <p className="text-center mb-0">
-                This website is operated by Van Mildert College Junior Common Room,
-                a charity (CIO) registered in England and Wales
-                (<a href="https://register-of-charities.charitycommission.gov.uk/charity-search/-/charity-details/5161238"
-                    target="_blank">1190990</a>).
-                Copyright Van Mildert College JCR.
-            </p>
-        </div>
-        {/* IMPORT FROM foot.html */}
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossOrigin="anonymous" async></script>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous" async></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous" async></script>
-        <script src="/assets/js/colourmode.js" async></script>
+        <Footer />
+        <Foot />
       </body>
     </html>
   )
