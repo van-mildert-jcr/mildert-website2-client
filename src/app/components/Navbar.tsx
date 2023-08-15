4e617a32fe2502c9745032a174ed501877ae1694
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 00:51:00 +0100

    use next.js Links instead of anchor tags

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,105 +15,105 @@ export default function Navbar() {
 
       <nav className="navbar bg-body-tertiary fixed-top navbar-expand-xl">
           <div className="container-fluid">
-              <a className="navbar-brand" href="/"><img src="/assets/img/Shield.svg" height="30px" width="auto" alt="VMJCR" /></a>
+              <Link className="navbar-brand" href="/"><img src="/assets/img/Shield.svg" height="30px" width="auto" alt="VMJCR" /></Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation">
                   <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navigation">
                   <div className="navbar-nav me-auto">
-                      <a id="menu-home" className="nav-item nav-link" href="/">Home</a>
+                      <Link id="menu-home" className="nav-item nav-link" href="/">Home</Link>
                       <div className="nav-item dropdown">
-                          <a id="menu-about" className="nav-link dropdown-toggle"
+                          <div id="menu-about" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               About Us
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-about">
-                              <a className="dropdown-item" href="/thejcr.php">What is the JCR?</a>
-                              <a className="dropdown-item" href="/purpose.php">JCR Purpose</a>
-                              <a className="dropdown-item" href="/whoswho.php">Who's Who</a>
-                              <a className="dropdown-item" href="/faq.php">FAQ</a>
-                              <a className="dropdown-item" href="/contact.php">Contact Us</a>
-                              <a className="dropdown-item" href="/charity.php">Charity Details</a>
+                              <Link className="dropdown-item" href="/thejcr.php">What is the JCR?</Link>
+                              <Link className="dropdown-item" href="/purpose.php">JCR Purpose</Link>
+                              <Link className="dropdown-item" href="/whoswho.php">Who's Who</Link>
+                              <Link className="dropdown-item" href="/faq.php">FAQ</Link>
+                              <Link className="dropdown-item" href="/contact.php">Contact Us</Link>
+                              <Link className="dropdown-item" href="/charity.php">Charity Details</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-gov" className="nav-link dropdown-toggle"
+                          <div id="menu-gov" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               Governance
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-gov">
-                              <a className="dropdown-item" href="/constitution">The Constitution</a>
-                              <a className="dropdown-item" href="/voting.php">Voting</a>
+                              <Link className="dropdown-item" href="/constitution">The Constitution</Link>
+                              <Link className="dropdown-item" href="/voting.php">Voting</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-college" className="nav-link dropdown-toggle"
+                          <div id="menu-college" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               The College
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-college">
-                              <a className="dropdown-item" href="/accommodation.php">Accommodation</a>
-                              <a className="dropdown-item" href="/bar.php">The Bar</a>
-                              <a className="dropdown-item" href="/gym.php">The Gym</a>
-                              <a className="dropdown-item" href="/studyspaces.php">Study Spaces</a>
-                              <a className="dropdown-item" href="/musicroom.php">Music Rooms</a>
-                              <a className="dropdown-item" href="/shop.php">Shop</a>
-                              <a className="dropdown-item" href="/tenniscourt.php">Tennis Courts</a>
+                              <Link className="dropdown-item" href="/accommodation.php">Accommodation</Link>
+                              <Link className="dropdown-item" href="/bar.php">The Bar</Link>
+                              <Link className="dropdown-item" href="/gym.php">The Gym</Link>
+                              <Link className="dropdown-item" href="/studyspaces.php">Study Spaces</Link>
+                              <Link className="dropdown-item" href="/musicroom.php">Music Rooms</Link>
+                              <Link className="dropdown-item" href="/shop.php">Shop</Link>
+                              <Link className="dropdown-item" href="/tenniscourt.php">Tennis Courts</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-fresher" className="nav-link dropdown-toggle"
+                          <div id="menu-fresher" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               Applicants & Freshers
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-fresher">
-                              <a className="dropdown-item" href="/whymildert.php">Why Mildert?</a>
-                              <a className="dropdown-item" href="/opendays.php">Open Days</a>
-                              <a className="dropdown-item" href="/freshersweek.php">Freshers' Week</a>
-                              <a className="dropdown-item" href="/fresheradvice.php">Fresher Advice</a>
-                              <a className="dropdown-item" href="/fresher_faq.php">Fresher FAQs</a>
+                              <Link className="dropdown-item" href="/whymildert.php">Why Mildert?</Link>
+                              <Link className="dropdown-item" href="/opendays.php">Open Days</Link>
+                              <Link className="dropdown-item" href="/freshersweek.php">Freshers' Week</Link>
+                              <Link className="dropdown-item" href="/fresheradvice.php">Fresher Advice</Link>
+                              <Link className="dropdown-item" href="/fresher_faq.php">Fresher FAQs</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-extra" className="nav-link dropdown-toggle"
+                          <div id="menu-extra" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               Extracurricular
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-extra">
-                              <a className="dropdown-item" href="/sports.php">Sports Clubs</a>
-                              <a className="dropdown-item" href="/committees_societies.php">Committees & Societies</a>
-                              <a className="dropdown-item" href="/outreach.php">Outreach Projects</a>
-                              <a className="dropdown-item" href="/interviewtips.php">Interview Technique</a>
+                              <Link className="dropdown-item" href="/sports.php">Sports Clubs</Link>
+                              <Link className="dropdown-item" href="/committees_societies.php">Committees & Societies</Link>
+                              <Link className="dropdown-item" href="/outreach.php">Outreach Projects</Link>
+                              <Link className="dropdown-item" href="/interviewtips.php">Interview Technique</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-events" className="nav-link dropdown-toggle"
+                          <div id="menu-events" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               Events
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-events">
                               <p className="mb-0 ps-3 fst-italic">Our events include:</p>
-                              <a className="dropdown-item" href="/formals.php">Formals</a>
-                              <a className="dropdown-item" href="/bops.php">BOPs</a>
-                              <a className="dropdown-item" href="/balls.php">Balls</a>
-                              <a className="dropdown-item" href="/fashionshow.php">Charity Fashion Show</a>
-                              <a className="dropdown-item" href="/48hour.php">48 Hour Musical</a>
+                              <Link className="dropdown-item" href="/formals.php">Formals</Link>
+                              <Link className="dropdown-item" href="/bops.php">BOPs</Link>
+                              <Link className="dropdown-item" href="/balls.php">Balls</Link>
+                              <Link className="dropdown-item" href="/fashionshow.php">Charity Fashion Show</Link>
+                              <Link className="dropdown-item" href="/48hour.php">48 Hour Musical</Link>
                           </div>
                       </div>
                       <div className="nav-item dropdown">
-                          <a id="menu-tech" className="nav-link dropdown-toggle"
+                          <div id="menu-tech" className="nav-link dropdown-toggle"
                              role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                               Tech
-                          </a>
+                          </div>
                           <div className="dropdown-menu" aria-labelledby="menu-tech">
-                              <a className="dropdown-item" href="/techcomm.php">Technical Committee</a>
-                              <a className="dropdown-item" href="/techhires.php">Hires</a>
+                              <Link className="dropdown-item" href="/techcomm.php">Technical Committee</Link>
+                              <Link className="dropdown-item" href="/techhires.php">Hires</Link>
                           </div>
                       </div>
-                      <a id="menu-t&s" className="nav-item nav-link" href="/welfare.php">Talk and Support</a>
+                      <Link id="menu-t&s" className="nav-item nav-link" href="/welfare.php">Talk and Support</Link>
                       <ColourMode />
                   </div>
-                  <a href="/Dashboard">Dashboard</a>
+                  <Link href="/Dashboard">Dashboard</Link>
                   {/* WE WILL USE THIS LOL --> <login /> */}
                   {/* <?php if (\mildert\session\isLoggedIn()): ?>
                       <a className="btn btn-dark me-2" href="/dashboard">Member dashboard</a>
