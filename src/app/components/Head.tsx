e29a351d64032aca17ec831bf1fd03df08686f42
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 00:39:24 +0100

    make Head a component

---
+++ src/app/components/Head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
+      <noscript>Please enable JavaScript.</noscript>
+      <script src="https://kit.fontawesome.com/d07da7e4d9.js" crossOrigin="anonymous" async></script>
+    </head>
+  )
+}
