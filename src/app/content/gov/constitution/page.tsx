1a4af64e85cb49cb2af4fd811efd1a7fb44b9110
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 01:08:21 +0100

    add dummy pages for 'governance'

---
+++ src/app/content/gov/constitution/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function Constitution() {
+  return (
+    <main>
+    </main>
+  )
+}
---
+++ src/app/content/gov/voting/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function Voting() {
+  return (
+    <main>
+    </main>
+  )
+}
