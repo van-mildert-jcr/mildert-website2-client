16274b590a1d23b1b3d3b7823b8b9fa674d357ef
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 01:19:37 +0100

    add dummy pages for 'events'

---
+++ src/app/content/events/balls/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function BallEvents() {
+  return (
+    <main>
+    </main>
+  )
+}
---
+++ src/app/content/events/formals/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function FormalEvents() {
+  return (
+    <main>
+    </main>
+  )
+}
