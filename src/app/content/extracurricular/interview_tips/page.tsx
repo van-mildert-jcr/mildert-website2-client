c41bcf998ebf6a2245029045f82d1fe3df2c9c5d
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 01:12:16 +0100

    add dummy pages for 'extracurricular'

---
+++ src/app/content/extracurricular/interview_tips/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function InterviewTips() {
+  return (
+    <main>
+    </main>
+  )
+}
---
+++ src/app/content/extracurricular/sports/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+
+}
+
+export default function Sports() {
+  return (
+    <main>
+    </main>
+  )
+}
