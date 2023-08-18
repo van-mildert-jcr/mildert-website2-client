2829e3ea20f845e3c6db9cb309fa5aeddfd88aba
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-19 00:48:09 +0100

    add temp content to who's who page

--- src/app/content/about/whos-who/page.tsx
+++ src/app/content/about/whos-who/page.tsx
@@ -1,10 +1,13 @@
 export const metadata = {
-
+  title: "Who's Who",
+  description: "Info on all prominent members of Van Mildert JCR",
 }
 
 export default function WhosWho() {
   return (
     <main>
+      <h1>Who's Who</h1>
+      <strong>This page's content isn't ready just yet - check back soon!</strong>
     </main>
   )
 }
