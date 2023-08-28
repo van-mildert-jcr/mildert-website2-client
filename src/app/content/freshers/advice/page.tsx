7133ef4fcfa954ca0131abafb79c2a00d612f7bf
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-28 18:07:11 +0100

    import freshers advice page

--- src/app/content/freshers/advice/page.tsx
+++ src/app/content/freshers/advice/page.tsx
@@ -1,10 +1,218 @@
 export const metadata = {
-
+  title: "Freshers Advice",
+  description: "Advice for Van Mildert Freshers",
 }
 
 export default function FresherAdvice() {
   return (
     <main>
+      <h1 className="mildert-page-heading">
+        Fresher Advice
+      </h1>
+      <p className="lead">
+        Freshers' week is an exciting and hectic time, stuffed to the brim with activities for Freshers to get to know
+        the college, university and their peers. It's a scary time, and we've all been through it, so here's a bit of
+        advice to get you through those first few weeks!
+      </p>
+      <hr/>
+      <div className="container">
+        <div className="text-center">
+          <h2>Top 3 Things To Remember</h2>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6">
+            <div className="d-flex align-items-center flex-column text-center">
+              <div className="p-2 bd-highlight">
+                <h3 className="title-v3-md text-uppercase">
+                  We've got your back
+                </h3>
+                <p>
+                  University is a big change from school and we know that. As Freps, we are here to support
+                  and help you. If you have a problem, trust us to give you a hand and don't think you have to
+                  go it alone. We're here for you.
+                </p>
+              </div>
+              <div className="p-2 bd-highlight">
+                <h3 className="title-v3-md text-uppercase">
+                  Give it a go!
+                </h3>
+                <p>
+                  There are lots of events and activities at Mildert, advertising societies, sports and more.
+                  The great thing about college is that you don't need to have touched a ball in your life to
+                  try out a new sport or have acting experience to join MildARTS. Try out something new and
+                  don't be scared to give it all a go!
+                </p>
+              </div>
+              <div className="p-2 bd-highlight">
+                <h3 className="title-v3-md text-uppercase">
+                  Don't feel pressured
+                </h3>
+                <p>
+                  Freshers' week caters for all. From the all-night party goers to the 9pm breakfast-tea club,
+                  there are social events available for everyone. Don't feel pressured to go out or stay in if
+                  you don't want to, because there will be plenty of people who are feeling the same as you
+                  who you can get to know at the events that suit YOU.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 d-flex align-items-center flex-column">
+            <div className="thumbnail-img">
+              <img className="img-responsive" src="/assets/img/masonry/footie.jpg" alt="" style={{width: "100%"}} />
+              <img className="img-responsive" src="/assets/img/masonry/rugby.jpg" alt="" style={{float: "left", width: "50%"}} />
+              <img className="img-responsive" src="/assets/img/masonry/rugby2.jpg" alt="" style={{width: "50%"}} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <h2>Here are some things we suggest you bring along:</h2>
+      <table className="table table-bordered">
+        <tbody>
+        <tr>
+          <th scope="row">
+            A Level/IB notes
+          </th>
+          <td>
+            These might come in handy during the first few weeks of your course
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Battery-operated fairy lights
+          </th>
+          <td>
+            Note only battery-operated sets are allowed for health and safety purposes (we know 🥲)
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            White tack/push-pins
+          </th>
+          <td>
+            <p>
+              You'll get a poster or two in your Fresher's pack, but if you want to add a bit of personality to your
+              room with something cute, white tack is an essential. Note only one accomodation block (Wear) has pin-boards,
+              with the exception of some rooms in Tees block.
+            </p>
+            <p style={{marginBottom: "0"}}>
+              White tack is preferable over blue tack as blue tack is known to leave stains.
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Bottle opener
+          </th>
+          <td>
+            We don't want any broken teeth around college 😅
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Coat hangers
+          </th>
+          <td>
+            If you don't have a coat to hang on them, you're insane. Or northern?
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Fancy dress
+          </th>
+          <td>
+            A requirement for the majority of social events
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Keyring/Lanyard
+          </th>
+          <td>
+            You'll get one during Freshers' fair, but losing your key on arrival day is true misery 😔.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Klute (clubbing) Shoes
+          </th>
+          <td>
+            A pair of shoes for your unavoidable nights out to Klute, the alleged worst nightclub in Europe (after the
+            actual worst burnt down). Dark-coloured are best, no one knows quite what those floors are covered in 😰
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Paracetamol/Throat lozenges
+          </th>
+          <td>
+            Freshers' flu is real. Even the strongest of us can't avoid it. You'll know when you can't hear your
+            lecturer over the sound of people coughing.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Pre-drinks cup and separate mug
+          </th>
+          <td>
+            Using the same cup for your tea and vodka mixer is not a good idea. Trust us.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Reed diffuser
+          </th>
+          <td>
+            Candles aren't allowed in college rooms because of the sensitivity of the fire alarms, so get one of
+            these bad boys to make your room smell as fresh as you guys are. On the note of sensitive fire alarms, be
+            careful of aerosol cans underneath them too...
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Smart matriculation clothes
+          </th>
+          <td>
+            Because alas, we are not a gown-wearing college, we have to try and show the other colleges up in some way
+            at Matriculation on Wednesday of Fresher's week.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Bluetooth speaker
+          </th>
+          <td>
+            Great for pre-drinks and corridor social time. Please, not too loud during exam season, though 😭
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Toothbrush
+          </th>
+          <td>
+            Someone would forget if it wasn't included.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Umbrella
+          </th>
+          <td>
+            It rains. This is England, after all.
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            Water-bottle
+          </th>
+          <td>
+            A necessity for trips to Maiden Castle or those 4am drunken awakenings. There's nothing worse than
+            Freshers' dodgeball with a hangover.
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </main>
-  )
+  );
 }
