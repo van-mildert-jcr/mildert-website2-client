2a761d561bbbbe5fa57e571d1e655ca1733f434c
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-28 17:19:54 +0100

    import music room page

--- src/app/content/college/music-room/page.tsx
+++ src/app/content/college/music-room/page.tsx
@@ -5,6 +5,29 @@ export const metadata = {
 export default function MusicRoom() {
   return (
     <main>
+      <h1 className="mildert-page-heading text-center mb-4">
+        Music Room
+      </h1>
+      <p className="lead text-center">
+        At Mildert, we're very lucky to have a recording studio on site, as well as 2 music practice rooms. Both of
+        these facilities are available to all Van Mildert students free of charge - bookings made at reception (around
+        events time they get very busy).
+        <br/>
+        The suite is available for use by any Music Department student (but no-one seems to realise this). Music
+        students have priority on weekdays (9am -6pm) and Mildert students at all other times.
+        <br/>
+        The Large practice room has upright piano and drum kit. One bass amp belongs to the JCR. Big Band and most
+        productions use this. All other amps are privately owned and are stored AT STUDENTS OWN RISK.
+        <br/>
+        The small practice room has baby grand piano. Both practice rooms are wired into the control room. This
+        has a 24-channel mixing desk and quad-core computer for making digital recordings and CDs. A range of
+        microphones, stands and cables are available for making recordings.
+        <br/>The Ustinov Room contains the Bechstein grand piano, built sometime between 1901-1914. It was recently
+        restored and is available for use by JCR pianists of Grade 8 standard.
+        <br/>
+        Recitals are organised fairly regularly, giving students the opportunity to perform in public on this
+        unique piano.
+      </p>
     </main>
   )
 }
