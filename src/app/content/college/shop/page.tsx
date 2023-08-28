0bef79f53e4d1cee7df942a0cc63568530374334
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-28 17:21:08 +0100

    import shop page

--- src/app/content/college/shop/page.tsx
+++ src/app/content/college/shop/page.tsx
@@ -1,10 +1,30 @@
 export const metadata = {
-
+  title: "Shop",
+  description: "Van Mildert College Shop",
 }
 
 export default function Shop() {
   return (
     <main>
+      <h1 className="mildert-page-heading mb-4">
+        Van Mildert College<br/>College Shop
+      </h1>
+      <p>
+        Mildert’s College Shop aims to fulfil all of your snacking needs, and is located right next to the college
+        bar. We sell a variety of confectionary, toasties, crisps, and drinks, as well as toiletries and household
+        goods.
+        <br/>The shop is run by the JCR Shop Committee, which is comprised of 7 members including the manager.
+        Interviews to apply to be on shop are held over the summer each year once a manager is appointed in late
+        easter term.
+        <br/>Members work a weekly four hour shift, as well as holding an extra voluntary role, such as filling
+        the vending machines, and running shop’s social media pages.
+      </p>
+      <hr/>
+      <h2>Opening Times (as of 23 Nov 2022)</h2>
+      <h3>
+        Monday - Sunday
+        <br/>7:30pm -11:15pm
+      </h3>
     </main>
   )
 }
