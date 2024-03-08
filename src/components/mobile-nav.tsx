35e77ac72e7c32b669100e8e012a87f1f7c9b02a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-08 15:19:34 +0000

    create placeholder mobile nav

---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,15 @@
+/*
+The contents of this file are largely based upon
+https://github.com/shadcn-ui/ui/blob/1f0a7008d6194e2c45b4ec250683e155f34be13f/apps/www/components/mobile-nav.tsx
+Copyright (c) 2023 shadcn
+shadcn licenses the referenced file to Van Mildert JCR under the MIT license.
+
+Copyright (c) 2024 Van Mildert JCR
+All rights reserved.
+ */
+
+export function MobileNav() {
+  return (
+    <div/>
+  )
+}
