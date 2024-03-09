bcd988616ce7f55714f9e0fd4090566236723869
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-09 08:49:15 +0000

    components should not be exported as `default`

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -14,7 +14,7 @@ import { MainNav } from "@/components/main-nav"
 import { MobileNav } from "@/components/mobile-nav"
 import { ColourModeToggle } from "@/components/colour-mode-toggle"
 
-export default function SiteHeader() {
+export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-bord  er/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
