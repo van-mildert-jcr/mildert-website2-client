5a1358d37db4a459476f713b94c76510abdeb079
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-09 07:56:39 +0000

    increase displayed logo size

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -21,7 +21,7 @@ import { Icons } from "@/components/icons"
 function SiteTitle() {
   return (
     <Link href="/" className="mr-6 flex items-center space-x-2">
-      <Icons.logo className="h-6 w-6" />
+      <Icons.logo className="h-12 w-12" />
       <span className="hidden font-bold sm:inline-block">
         {siteConfig.name}
       </span>
@@ -38,19 +38,22 @@ export function MainNav() {
   const pathname = usePathname()
 
   function NavLink(props: NavLinkProps) {
+
+    const { activePathnamePrefix, children, ...restProps } = props;
+
     return (
       <Link
-        {...props}
+        {...restProps}
         className={
           cn(
             "transition-colors hover:text-foreground/80",
-            pathname?.startsWith(props.activePathnamePrefix)
+            pathname?.startsWith(activePathnamePrefix)
               ? "text-foreground"
               : "text-foreground/60"
           )
         }
       >
-        {props.children}
+        {children}
       </Link>
     )
   }
