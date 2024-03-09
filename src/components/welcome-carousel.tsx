755739fe9d6dc3a52719bb10ad5d82c1fc206d93
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-09 21:59:32 +0000

    populate the welcome carousel using the embla carousel component

--- src/components/welcome-carousel.tsx
+++ src/components/welcome-carousel.tsx
@@ -1,20 +1,39 @@
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@/components/ui/carousel";
 
 export function WelcomeCarousel() {
   return (
-    <div className="bg-white">
-      <div className="flex">
-        <div className="w-1/2">
-          <div className="w-full h-full relative">
-            <Image src="/next.svg" alt="College Building" fill style={{objectFit: "contain"}}/>
-          </div>
-
-        </div>
-        <div className="w-1/2 bg-green-700 text-white p-16">
-          <h1 className="text-5xl font-bold mb-4">Welcome to the home of Van Mildert JCR.</h1>
-          <div className="w-24 h-1 bg-yellow-400"/>
-        </div>
+    <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="lg:col-span-2">
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>
+              <Image src="/photos/college-feather.png" alt="Photo of the feather sculpture at Van Mildert College" width={1200} height={800}/>
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/photos/college-feather.png" alt="Photo of the feather sculpture at Van Mildert College" width={1200} height={800}/>
+            </CarouselItem>
+            <CarouselItem>
+              <Image src="/photos/college-feather.png" alt="Photo of the feather sculpture at Van Mildert College" width={1200} height={800}/>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious/>
+          <CarouselNext/>
+          <CarouselDots/>
+        </Carousel>
       </div>
-    </div>
+      <section className="bg-yellow-400 pl-4">
+        <div className="h-full flex flex-col items-center justify-center ">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Welcome to the home of Van Mildert JCR.</h1>
+        </div>
+      </section>
+    </article>
   )
 }
