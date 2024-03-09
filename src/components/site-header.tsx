5cd824e63b1c43e5ae17a6d87718c712dbd1bddb
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-09 21:59:56 +0000

    tweak comments for accuracy

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -17,17 +17,17 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-bord  er/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        {/* Left-aligned nav & mobile nav short-circuit */}
+        {/* left-aligned nav (desktop only) */}
         <MainNav />
         <MobileNav />
 
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="w-full flex-1 md:w-auto md:flex-none">
-            {/* Right-aligned nav */}
+            {/* Persistent nav (desktop + mobile) */}
             <SecondaryNav />
           </div>
 
-          {/* Socials & colour mode toggle */}
+          {/* Icon nav (socials, colour mode, dashboard) */}
           <nav className="flex items-center">
             <ColourModeToggle />
           </nav>
