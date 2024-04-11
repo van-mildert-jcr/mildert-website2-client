d768ac55354bc91f312e9f6bcf9718552e0ed107
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-11 20:00:17 +0100

    sort imports

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -14,10 +14,10 @@ import * as React from "react"
 import Link, {LinkProps} from "next/link"
 import { usePathname } from "next/navigation"
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { Button } from "@/components/ui/button";
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
 
 function SiteTitle() {
   return (
