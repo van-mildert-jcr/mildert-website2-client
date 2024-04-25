3fa30f53645954c5b1127f3b29e797d4a0084a00
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-25 23:22:51 +0100

    cheeky little helper to make applying next.js config plugins tidier

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,48 @@
 import { createContentlayerPlugin } from "next-contentlayer2"
 
-/** @type {import('next').NextConfig} */
+/**
+ * @typedef {import('next').NextConfig} NextConfig
+ */
+
+/** @type {NextConfig} */
 const nextConfig = {
 };
 
 const withContentlayer = createContentlayerPlugin({
   // Additional Contentlayer config options
-})
+});
 
-export default withContentlayer(nextConfig);
+/**
+ * @callback NextPluginApplicator
+ * @param {NextConfig} nextConfig
+ * @returns {NextConfig}
+ */
+
+class ConfigBuilder {
+  /**
+   * @param {NextConfig} config
+   */
+  constructor(config) {
+    this.config = config;
+  }
+
+  /**
+   * @param {NextPluginApplicator} pluginApplicator
+   * @returns {ConfigBuilder}
+   */
+  apply(pluginApplicator) {
+    this.config = pluginApplicator(this.config);
+    return this;
+  }
+
+  /**
+   * @returns {NextConfig}
+   */
+  get() {
+    return this.config;
+  }
+}
+
+export default new ConfigBuilder(nextConfig)
+  .apply(withContentlayer)
+  .get()
