5bb06e2f7b8662659f0f69d6727f88cb7600e2cb
Alastair Hargreaves-McManus <2028319+ahargreavesmcmanus@users.noreply.github.com>
2024-05-09 12:13:00 +0100

    Set lang as British English

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en-GB">
       <body className={cn(inter.className, "min-h-screen antialiased bg-background")}>
         <ThemeProvider
           attribute="class"
